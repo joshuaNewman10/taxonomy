993e79c51f9d9469773766ec241bc16d8a621781
Joshua Newman <jonewman1020@gmail.com>
2015-02-09 20:48:49 -0800

    Finished assignment

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamle';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.constructor = ForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+ return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
